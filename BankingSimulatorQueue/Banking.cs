d16daf929fd42721ba3a70f820512ff7d83a35de
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-05 21:34:05 +0530

    Added: cash withdraw and acsh deposit logic part.

---
+++ BankingSimulatorQueue/Banking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSimulatorQueue
+{
+    // cash withdraw and deposite are done
+    public class Banking
+    {
+        // cash withdraw
+        public int cashWithdraw(int cashToWithdraw, int currentBalance)
+        {
+            currentBalance -= cashToWithdraw;
+            return currentBalance;
+        }
+
+        // cash deposite
+        public int cashDeposite(int cashToDeposite, int currentBalance)
+        {
+            currentBalance += cashToDeposite;
+            return currentBalance;
+        }
+    }
+}
