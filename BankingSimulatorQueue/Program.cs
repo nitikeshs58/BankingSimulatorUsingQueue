50185480a59d7fb3b30129629bba94a5e3fb2a4a
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-05 17:00:22 +0530

    Added: Welcome message to Banking Simulator using queue.

---
+++ BankingSimulatorQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingSimulatorQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!**Welcome to cash counter simulator Using Queue list.");
+        }
+    }
+}
